7c572f8563048de8540623635e23c7d3a4bcc659
AziziNyawa <nyawaazizi@gmail.com>
2023-03-27 15:49:09 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/*
+ * _strlen: returns the lenghth of a string
+ *
+ * Return: int
+ *
+ */int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
