42f1dc4a08dd689b230316472b93397221ceb682
AziziNyawa <nyawaazizi@gmail.com>
2023-04-03 13:56:00 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strchr - finds character c in string s
+ * @s: the string where the character c is searched
+ * @c: the character to be searched in s
+ * Return: the string plus the index when c is found
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0 ; s[i] != '\0' ; i++)
+		if (s[i] == c)
+			return (s + i);
+	return ('\0');
+}
