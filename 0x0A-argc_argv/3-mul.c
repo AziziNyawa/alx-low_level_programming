0b8c9899783ae0f4fedffc829909a701f78c7575
AziziNyawa <nyawaazizi@gmail.com>
2023-04-09 14:22:51 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,20 +8,21 @@
  * Return: its return i when argc is less then 2
  * and 0 when argc is 3
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	int n1 = 0, n2 = 0;
+	int n, m, diff;
 
-	if (argc == 3)
+	if (argc != 3)
 	{
-		n1 = atoi(argv[1]);
-		n2 = atoi(argv[2]);
-		printf("%d\n", n1 * n2);
-	}
-	else
-	{
-		printf("Error");
+		printf("Error\n");
 		return (1);
 	}
+
+	n = atoi(argv[1]);
+	m = atoi(argv[2]);
+	diff = n * m;
+
+	printf("%i\n", diff);
+
 	return (0);
 }
