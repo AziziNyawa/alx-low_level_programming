8856d91176f82800669c0194e12c7d67833242e8
AziziNyawa <nyawaazizi@gmail.com>
2023-04-09 09:30:29 -0700

    my lib

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,17 +1,18 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * _puts - function to print string to stdout
+ * @str: string
+ * Return: Always 0
  */
 void _puts(char *str)
 {
-int i = 0;
-while (str[i])
-{
-_putchar(str[i]);
-i++;
-}
-_putchar('\n');
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
 }
